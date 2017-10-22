b0058fffabeed9a9c4f93870acdfc396d035967b
Pickausername2017 <31811968+Pickausername2017@users.noreply.github.com>
2017-10-22 09:32:54 -0400

    Add files via upload

---
+++ M5LAB1.py
@@ -0,0 +1,22 @@
+# CTI-110 
+# M5 Lab1 - Discount Calculator
+# Denise Bruce
+# 21 Oct 2017
+# This program draws a square and  triangle using a for loop. 
+#
+def main ():
+        import turtle
+        for i in [0,1,2,3,]:
+                turtle.forward(50)
+                turtle.left(90)
+	
+	
+        for i in [0,1]:
+                turtle.forward(50)
+                turtle.right(60)
+        for i in [2]:
+                turtle.right(90)
+                turtle.forward(100)
+	
+                
+main() 
